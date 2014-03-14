c82b7e55c7bbb2cab939bdf8c3efeb1fb0ea0384
Drew <aer10f.ar@gmail.com>
2014-03-14 18:47:09 -0400

    transition for search bar working!

--- app/src/main/java/UI_Fragments/GoalManager.java
+++ app/src/main/java/UI_Fragments/GoalManager.java
@@ -7,121 +7,93 @@ import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.app.R;
+
+import java.util.Random;
 
 /**
  * Created by Drew on 3/13/14.
  */
 public class GoalManager extends Fragment {
     View rootView;
-    LinearLayout grid;
+    LinearLayout leftGrid, rightGrid;
+    boolean leftFirst;
+    Button sortBy;
+    Random r;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.goal_manager_layout, container, false);
-        grid = (LinearLayout) rootView.findViewById(R.id.goalsGrid);
-        grid.addView(createLargeBlock());
-        grid.addView(createTwoMediumBlockRow());
-        grid.addView(createOneMediumTwoSmallBlockRow());
-        grid.addView(createThreeSmallBlockRow());
+        leftGrid = (LinearLayout) rootView.findViewById(R.id.goalsLeftGrid);
+        rightGrid = (LinearLayout) rootView.findViewById(R.id.goalsRightGrid);
+        sortBy = (Button) rootView.findViewById(R.id.sortByButton);
+        leftFirst = true;
+
+        r = new Random();
+
+        addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+        addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+        addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+        addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+        addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+        addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+        addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+        addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+
+        sortBy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leftGrid.removeAllViews();
+                rightGrid.removeAllViews();
+
+                addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+                addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+                addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+                addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+                addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+                addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+                addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+                addGoal(createBlock(r.nextInt(3)), leftGrid, rightGrid);
+            }
+        });
 
         return rootView;
     }
 
-    private LinearLayout createLargeBlock()
+    private void addGoal(LinearLayout block, LinearLayout left, LinearLayout right) {
+        if(leftFirst)
+        {
+            left.addView(block);
+            leftFirst = false;
+        }
+        else
+        {
+            right.addView(block);
+            leftFirst = true;
+        }
+    }
+
+    private LinearLayout createBlock(int size)
     {
+        while(size == 0)
+            size = r.nextInt(3);
+
         DisplayMetrics metrics = getActivity().getApplicationContext().getResources().getDisplayMetrics();
         LinearLayout lb = new LinearLayout(getActivity().getApplicationContext());
         lb.setOrientation(LinearLayout.VERTICAL);
-        lb.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, Math.round(metrics.heightPixels/3)));
+        lb.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, Math.round(metrics.heightPixels/5)*size));
         lb.setBackgroundColor(Color.BLACK);
 
         TextView lt = new TextView(getActivity().getApplicationContext());
-        lt.setText("large block");
+        lt.setText("block of size: " + size);
 
         lb.addView(lt);
-
-        return lb;
-    }
-    private LinearLayout createMediumBlock(int weight)
-    {
-        DisplayMetrics metrics = getActivity().getApplicationContext().getResources().getDisplayMetrics();
-        LinearLayout lb = new LinearLayout(getActivity().getApplicationContext());
-        lb.setOrientation(LinearLayout.VERTICAL);
-        //LinearLayout.LayoutParams params = new LinearLayout.LayoutParams((int)Math.round(metrics.widthPixels*.64), Math.round(metrics.heightPixels/3));
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.FILL_PARENT);
-        params.weight = weight;
-        lb.setLayoutParams(params);
-        lb.setBackgroundColor(Color.BLUE);
-
-        TextView lt = new TextView(getActivity().getApplicationContext());
-        lt.setText("medium block");
-
-        lb.addView(lt);
-
-        return lb;
-    }
-    private LinearLayout createSmallBlock(int weight)
-    {
-        DisplayMetrics metrics = getActivity().getApplicationContext().getResources().getDisplayMetrics();
-        LinearLayout lb = new LinearLayout(getActivity().getApplicationContext());
-        lb.setOrientation(LinearLayout.VERTICAL);
-        //LinearLayout.LayoutParams params = new LinearLayout.LayoutParams((int)Math.round(metrics.widthPixels*.335), Math.round(metrics.heightPixels/3)));
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.FILL_PARENT);
-        params.weight = weight;
-        lb.setLayoutParams(params);
-        lb.setBackgroundColor(Color.GREEN);
-
-        TextView lt = new TextView(getActivity().getApplicationContext());
-        lt.setText("small block");
-
-        lb.addView(lt);
-
-        return lb;
-    }
-
-    private LinearLayout createTwoMediumBlockRow()
-    {
-        DisplayMetrics metrics = getActivity().getApplicationContext().getResources().getDisplayMetrics();
-        LinearLayout lb = new LinearLayout(getActivity().getApplicationContext());
-        lb.setOrientation(LinearLayout.HORIZONTAL);
-        lb.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, Math.round(metrics.heightPixels/3)));
-
-        lb.addView(createMediumBlock(1));
-        lb.addView(createMediumBlock(1));
-
-        return lb;
-    }
-
-    private LinearLayout createOneMediumTwoSmallBlockRow()
-    {
-        DisplayMetrics metrics = getActivity().getApplicationContext().getResources().getDisplayMetrics();
-        LinearLayout lb = new LinearLayout(getActivity().getApplicationContext());
-        lb.setOrientation(LinearLayout.HORIZONTAL);
-        lb.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, Math.round(metrics.heightPixels/3)));
-        lb.setWeightSum(7);
-
-        lb.addView(createMediumBlock(3));
-        lb.addView(createSmallBlock(2));
-        lb.addView(createSmallBlock(2));
-
-        return lb;
-    }
-
-    private LinearLayout createThreeSmallBlockRow()
-    {
-        DisplayMetrics metrics = getActivity().getApplicationContext().getResources().getDisplayMetrics();
-        LinearLayout lb = new LinearLayout(getActivity().getApplicationContext());
-        lb.setOrientation(LinearLayout.HORIZONTAL);
-        lb.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, Math.round(metrics.heightPixels/3)));
-
-        lb.addView(createSmallBlock(1));
-        lb.addView(createSmallBlock(1));
-        lb.addView(createSmallBlock(1));
 
         return lb;
     }
