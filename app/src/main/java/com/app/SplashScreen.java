be097c6c5f176ddcaf10b2dbabb576c871758432
eannelson <eannelson2010@gmail.com>
2014-03-18 20:47:18 -0400

    3/18 - Ean
    Added Splash Screen Layout and Activity

---
+++ app/src/main/java/com/app/SplashScreen.java
@@ -0,0 +1,44 @@
+package com.app;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+/**
+ * Created by eanne_000 on 3/18/14.
+ */
+public class SplashScreen extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        actionBar = getActionBar();
+        actionBar.hide();
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+
+}
