1738c444dae42604217211d2609167639a555e2e
Drew <aer10f.ar@gmail.com>
2014-03-21 20:18:52 -0400

    made adding a goal async

--- app/src/main/java/GoalFragments/Goal.java
+++ app/src/main/java/GoalFragments/Goal.java
@@ -1,10 +1,15 @@
 package GoalFragments;
 
 import android.app.Fragment;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.app.R;
@@ -18,6 +23,7 @@ public class Goal extends Fragment {
 
     View rootView;
     LinearLayout container;
+    ImageView goalImage;
     int minHeight;
 
     public Goal()
@@ -36,15 +42,60 @@ public class Goal extends Fragment {
         switch(minHeight){
             case 0:
                 rootView = inflater.inflate(R.layout.goal_display_layout1, container, false);
+                goalImage = (ImageView) rootView.findViewById(R.id.GoalViewContainer);
+                goalImage.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.violin, 250, 250));
                 break;
             case 1:
                 rootView = inflater.inflate(R.layout.goal_display_layout2, container, false);
+                goalImage = (ImageView) rootView.findViewById(R.id.GoalViewContainer);
+                goalImage.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.marathon, 250, 250));
                 break;
             case 2:
                 rootView = inflater.inflate(R.layout.goal_display_layout3, container, false);
+                goalImage = (ImageView) rootView.findViewById(R.id.GoalViewContainer);
+                goalImage.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.recording_studio, 250, 250));
                 break;
         }
 
         return rootView;
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+        int reqWidth, int reqHeight) {
+
+            // First decode with inJustDecodeBounds=true to check dimensions
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeResource(res, resId, options);
+
+            // Calculate inSampleSize
+            options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+            // Decode bitmap with inSampleSize set
+            options.inJustDecodeBounds = false;
+            return BitmapFactory.decodeResource(res, resId, options);
     }
 }
--- app/src/main/java/UI_Fragments/GoalManager.java
+++ app/src/main/java/UI_Fragments/GoalManager.java
@@ -2,10 +2,15 @@ package UI_Fragments;
 
 import android.annotation.TargetApi;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.BoringLayout;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,9 +30,10 @@ import GoalFragments.Goal;
 public class GoalManager extends Fragment {
     View rootView;
     LinearLayout leftGrid, rightGrid;
-    boolean leftFirst;
     Button sortBy;
     Random r;
+    int leftHeight, rightHeight, rand;
+    boolean left;
 
 
     @Override
@@ -37,37 +43,35 @@ public class GoalManager extends Fragment {
         leftGrid = (LinearLayout) rootView.findViewById(R.id.goalsLeftGrid);
         rightGrid = (LinearLayout) rootView.findViewById(R.id.goalsRightGrid);
         sortBy = (Button) rootView.findViewById(R.id.sortByButton);
-        leftFirst = true;
+
+        leftHeight = 0;
+        rightHeight = 0;
+        left = true;
 
         r = new Random();
 
-        addGoal(leftGrid, rightGrid);
-        addGoal(leftGrid, rightGrid);
-        addGoal(leftGrid, rightGrid);
-        addGoal(leftGrid, rightGrid);
-        addGoal(leftGrid, rightGrid);
-        addGoal(leftGrid, rightGrid);
-        addGoal(leftGrid, rightGrid);
-        addGoal(leftGrid, rightGrid);
-        addGoal(leftGrid, rightGrid);
-        addGoal(leftGrid, rightGrid);
+        addGoal();
+        addGoal();
+        addGoal();
+        addGoal();
+        addGoal();
+        addGoal();
+        addGoal();
 
         sortBy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 leftGrid.removeAllViews();
                 rightGrid.removeAllViews();
+                leftHeight = 0;
+                rightHeight = 0;
 
-                addGoal(leftGrid, rightGrid);
-                addGoal(leftGrid, rightGrid);
-                addGoal(leftGrid, rightGrid);
-                addGoal(leftGrid, rightGrid);
-                addGoal(leftGrid, rightGrid);
-                addGoal(leftGrid, rightGrid);
-                addGoal(leftGrid, rightGrid);
-                addGoal(leftGrid, rightGrid);
-                addGoal(leftGrid, rightGrid);
-                addGoal(leftGrid, rightGrid);
+                addGoal();
+                addGoal();
+                addGoal();
+                addGoal();
+                addGoal();
+                addGoal();
             }
         });
 
@@ -75,16 +79,57 @@ public class GoalManager extends Fragment {
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-    private void addGoal(LinearLayout left, LinearLayout right) {
-        if(leftFirst)
-        {
-            getChildFragmentManager().beginTransaction().add(R.id.goalsLeftGrid, new Goal(r.nextInt(3))).commit();
-            leftFirst = false;
+    private void addGoal() {
+        new addGoal().execute(getChildFragmentManager());
+    }
+
+    private class addGoal extends AsyncTask<FragmentManager,Integer, Long>
+    {
+        @Override
+        protected Long doInBackground(FragmentManager... fm) {
+            rand = r.nextInt(3);
+            if(left)
+            {
+                fm[0].beginTransaction().add(R.id.goalsLeftGrid, new Goal(rand)).setCustomAnimations(R.anim.enter_comment, R.anim.leave_comment).commit();
+                switch(rand)
+                {
+                    case 0:
+                        leftHeight += 150;
+                        break;
+                    case 1:
+                        leftHeight += 200;
+                        break;
+                    case 2:
+                        leftHeight += 250;
+                        break;
+                }
+                left = false;
+            }
+            else
+            {
+                fm[0].beginTransaction().add(R.id.goalsRightGrid, new Goal(rand)).setCustomAnimations(R.anim.enter_comment, R.anim.leave_comment).commit();
+                switch(rand)
+                {
+                    case 0:
+                        rightHeight += 150;
+                        break;
+                    case 1:
+                        rightHeight += 200;
+                        break;
+                    case 2:
+                        rightHeight += 250;
+                        break;
+                }
+                left = true;
+            }
+
+            return null;
         }
-        else
-        {
-            getChildFragmentManager().beginTransaction().add(R.id.goalsRightGrid, new Goal(r.nextInt(3))).commit();
-            leftFirst = true;
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+        }
+        @Override
+        protected void onPostExecute(Long result) {
         }
     }
 }
